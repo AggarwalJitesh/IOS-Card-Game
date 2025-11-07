0be5815c6976ea0b2502b8601e18e4fd158d34df
Jitesh Aggarwal <96955945+AggarwalJitesh@users.noreply.github.com>
2025-11-07 22:35:28 +0530

    Add files via upload

---
+++ Sync_A_Dating_AppApp.swift
@@ -0,0 +1,16 @@
+//
+//  Sync_A_Dating_AppApp.swift
+//
+//  Created by Jitesh Aggarwal on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Sync_A_Dating_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
